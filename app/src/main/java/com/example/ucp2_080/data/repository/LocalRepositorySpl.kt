db0143676ec17c2d88978f1a5fa79692676d00f4
Egin Pardewan <eginpardewan@gmail.com>
2024-12-18 20:28:11 +0700

    melanjutkan LocalRepositorySpl

--- app/src/main/java/com/example/ucp2_080/data/repository/LocalRepositorySpl.kt
+++ app/src/main/java/com/example/ucp2_080/data/repository/LocalRepositorySpl.kt
@@ -1,7 +1,23 @@
 package com.example.ucp2_080.data.repository
 
 import com.example.ucp2_080.data.dao.SuplierDao
+import com.example.ucp2_080.data.entity.Suplier
+import kotlinx.coroutines.flow.Flow
 
-class LocalRepositorySpl {
+class LocalRepositorySpl (
     private val suplierDao: SuplierDao
+) : RepositorySpl {
+    override suspend fun insertSpl(suplier: Suplier) {
+        suplierDao.insertSuplier(suplier)
+    }
+
+    //getAllSpl
+    override fun getAllSpl(): Flow<List<Suplier>> {
+        return suplierDao.getAllSuplier()
+    }
+
+    //getSpl
+    override fun getSpl(nama: String): Flow<Suplier> {
+        return suplierDao.getSuplier(nama)
+    }
 }
